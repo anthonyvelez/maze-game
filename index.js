c1f409a1475c2cb65a319dc12c860d8dc8f7e9c5
Anthony Velez <velezanthony1@gmail.com>
2020-03-22 15:51:52 -0400

    Add initial files and configure MatterJS

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const width = 600;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+	element : document.body,
+	engine  : engine,
+	options : {
+		wireframes : false,
+		width,
+		height
+	}
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+//Walls
+const walls = [
+	//top
+	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+	//bottom
+	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+	//left
+	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+	//right
+	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+];
+
+World.add(world, walls);
