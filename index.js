1d6c9a90629c552bc887b7128bb779fa370a87e5
Anthony Velez <velezanthony1@gmail.com>
2020-03-25 16:12:49 -0400

    Add maze generation algorithm

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -31,3 +32,91 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+//Maze generation
+const shuffle = (arr) => {
+	let counter = arr.length;
+
+	while (counter > 0) {
+		const index = Math.floor(Math.random() * counter);
+		counter--;
+
+		const temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
+	}
+
+	return arr;
+};
+
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+	//if we have visited the cell at [row, column] then return
+	if (grid[row][column]) {
+		return;
+	}
+	//Mark this cell at being visited
+	grid[row][column] = true;
+	//Assembele randomly ordered neighbors
+	const neighbors = shuffle([
+		[
+			row - 1,
+			column,
+			'up'
+		],
+		[
+			row,
+			column + 1,
+			'right'
+		],
+		[
+			row + 1,
+			column,
+			'down'
+		],
+		[
+			row,
+			column - 1,
+			'left'
+		]
+	]);
+
+	for (let neighbor of neighbors) {
+		const [
+			nextRow,
+			nextColumn,
+			direction
+		] = neighbor;
+
+		//Check if neighbor is out of bounds
+		if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+			continue;
+		}
+
+		//If we've visited neighbor, continue to next neighbor
+		if (grid[nextRow][nextColumn]) {
+			continue;
+		}
+
+		//Remove wall from verticals or horizontals array
+		if (direction === 'left') {
+			verticals[row][column - 1] = true;
+		} else if (direction === 'right') {
+			verticals[row][column] = true;
+		} else if (direction === 'up') {
+			horizontals[row - 1][column] = true;
+		} else if (direction === 'down') {
+			horizontals[row][column] = true;
+		}
+
+		stepThroughCell(nextRow, nextColumn);
+	}
+};
+
+stepThroughCell(startRow, startColumn);
